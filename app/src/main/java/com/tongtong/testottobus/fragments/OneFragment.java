b265cc2becb728ca28e6977b7866ba7efb2dccd8
ixoye63 <ixoye63@naver.com>
2016-07-06 15:58:31 +0900

    테스트 완료 : 3개 생산자와 1개 구독자 모델

--- app/src/main/java/com/tongtong/testottobus/fragments/OneFragment.java
+++ app/src/main/java/com/tongtong/testottobus/fragments/OneFragment.java
@@ -8,11 +8,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.tongtong.testottobus.EventBusCallTest;
+import com.squareup.otto.Subscribe;
+import com.tongtong.testottobus.BusProvider;
+import com.tongtong.testottobus.TestEvent1;
 import com.tongtong.testottobus.R;
-
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
+import com.tongtong.testottobus.TestEvent2;
+import com.tongtong.tonglib.util.LogUtil;
+import com.tongtong.tonglib.util.Util;
 
 
 public class OneFragment extends Fragment {
@@ -21,32 +23,92 @@ public class OneFragment extends Fragment {
     private int mPlusNum = 0;
 
     private TextView mTv;
-    private String mText;
+    private String mText = "";
+
+    private boolean isRegister = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup cotainer, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_one, cotainer, false);
+        LogUtil.d("");
 
+        View view = inflater.inflate(R.layout.fragment_one, cotainer, false);
         mTv = (TextView) view.findViewById(R.id.test_num);
+
+        initUi();
+        initEvent();
 
         return view;
     }
 
+    void initUi() {
+        LogUtil.d("");
+        mTv.setText(mText);
+    }
+
+    void initEvent() {
+        LogUtil.d("");
+        registerEventBus();
+    }
+
     @Subscribe
-    public void onEventMainThread(EventBusCallTest event){
-        mText = event.getTransNum() + "";
+    public void getMessageEvent(String message){
+        LogUtil.d("String >>> %s", message);
+
+        mText = message;
+        mTv.setText(mText);
+    }
+
+    @Subscribe
+    public void getMessageEvent(TestEvent1 event){
+        LogUtil.d("TestEvent1 >>> %s", event.getMessage());
+
+        mText = event.getMessage();
+        mTv.setText(mText);
+    }
+
+    @Subscribe
+    public void getMessageEvent(TestEvent2 event){
+        LogUtil.d("TestEvent2 >>> %s %d", event.getTransFrom(), event.getTransNum());
+
+        mText = Util.sprintf("%s %d", event.getTransFrom(), event.getTransNum());
         mTv.setText(mText);
     }
 
     @Override
     public void onStart() {
-        EventBus.getDefault().register(this);
+        LogUtil.d("");
+
         super.onStart();
+        registerEventBus();
     }
 
     @Override
     public void onStop() {
-        EventBus.getDefault().unregister(this);
+        LogUtil.d("");
+
         super.onStop();
+    }
+
+    @Override
+    public void onResume() {
+        LogUtil.d("");
+
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        LogUtil.d("");
+
+        super.onPause();
+    }
+
+    void registerEventBus() {
+        LogUtil.d("isRegister %b", isRegister);
+
+        if(!isRegister) {
+            BusProvider.getInstance().register(this);
+            isRegister = true;
+        }
     }
 }
